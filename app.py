41a625b28f7fd820d5eed481b14c626fce472d6f
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-05-18 12:23:33 +0100

    updates to db

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import sqlite3
+from hashids import Hashids
+
+# Hashids is a small open-source library that generates short, unique, non-sequential ids from numbers. It converts numbers like 347 into strings like “yr8”
+# alternative to Hashids would be bycrypt
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'your secret key'
+
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
--- url-shortener.sql
+++ url-shortener.sql
@@ -3,6 +3,13 @@
 DROP TABLE IF EXISTS urlshortener
 
 CREATE TABLE urlshortener (
-    original_url = TEXT NOT NULL
-    short_url TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    original_url TEXT NOT NULL,
+    clicks INTEGER NOT NULL DEFAULT 0
 )
+
+-- id: The ID of the URL, this will be a unique integer value for each URL entry. You will use it to get the original URL from a hash string.
+-- created: The date the URL was shortened.
+-- original_url: The original long URL to which you will redirect users.
+-- clicks: The number of times a URL has been clicked. The initial value will be 0, which will increment with each redirect.
